ce0dbe17f42a979be8adeffe8613b1c58267522e
Dmytro Morozov <mail@dmorozov.info>
2022-04-25 09:39:57 +0300

    Chapter 3.7: Handler for new subscriptions

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -29,3 +29,57 @@ async fn health_check_works() {
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
+
+#[actix_rt::test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    // Act
+    let response = client
+        // Use the returned application address
+        .post(&format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+}
+
+#[actix_rt::test]
+async fn subscribe_returns_400_when_data_is_missing() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    // Act
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            // Use the returned application address
+            .post(&format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Additional customized error message on test failure
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
